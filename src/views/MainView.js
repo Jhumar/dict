0816cd4bca2ad56e0138ca3b4d87b08e85364ca2
Ming-desu <joshuaming.jm@gmail.com>
2022-08-10 21:00:00 +0800

    added support of multiple videos and images

--- src/views/MainView.js
+++ src/views/MainView.js
@@ -9,7 +9,6 @@ import { useStateValue } from "../contexts/StateProvider";
 import { API_URL, GET } from "./../utils/axios";
 
 function MainView() {
-
   // date and time
 
   const QUEUE_PER_PAGE = 7;
@@ -23,8 +22,21 @@ function MainView() {
 
   const [{ source }, dispatch] = useStateValue();
   const [queues, setQueues] = useState([]);
-  const [ads, setAds] = useState([]);
+  const [ads, _setAds] = useState([]);
+
+  const adsRef = useRef(ads);
+  const setAds = (data) => {
+    adsRef.current = data;
+    _setAds(data);
+  };
+
   const [settings, setSettings] = useState([]);
+
+  const INDEX_OF_MEDIA_AT_SLOT_ONE = useRef(0);
+  const INDEX_OF_MEDIA_AT_SLOT_TWO = useRef(0);
+
+  const ELAPSED_TIME_AT_SLOT_ONE = useRef(0);
+  const ELAPSED_TIME_AT_SLOT_TWO = useRef(0);
 
   const getAds = () => {
     const { request, source } = GET("/media/p/r");
@@ -55,6 +67,84 @@ function MainView() {
     });
   };
 
+  const checkLoop = () => {
+    console.log("Looping");
+    // if ((adsRef.current.slot_one || []).length > 0) {
+    //   const currentMediaAtSlotOne =
+    //     adsRef.current.slot_one[INDEX_OF_MEDIA_AT_SLOT_ONE];
+
+    //   console.log(currentMediaAtSlotOne);
+
+    //   if (ELAPSED_TIME_AT_SLOT_ONE + 1 < currentMediaAtSlotOne.duration) {
+    //     ELAPSED_TIME_AT_SLOT_ONE.current += 1;
+    //   } else {
+    //     INDEX_OF_MEDIA_AT_SLOT_ONE.current =
+    //       INDEX_OF_MEDIA_AT_SLOT_ONE.current + 1 <
+    //       adsRef.current.slot_one.length
+    //         ? INDEX_OF_MEDIA_AT_SLOT_ONE.current + 1
+    //         : 0;
+    //     ELAPSED_TIME_AT_SLOT_ONE.current = 0;
+    //   }
+    // }
+
+    if (adsRef.current.length <= 0) return;
+
+    const slot_one = adsRef.current.slot_one || [];
+    const slot_two = adsRef.current.slot_two || [];
+
+    if (slot_one.length > 0) {
+      const currentMediaAtSlotOne =
+        slot_one[INDEX_OF_MEDIA_AT_SLOT_ONE.current];
+
+      if (
+        ELAPSED_TIME_AT_SLOT_ONE.current + 1 <
+        currentMediaAtSlotOne.duration
+      ) {
+        ELAPSED_TIME_AT_SLOT_ONE.current += 1;
+      } else {
+        INDEX_OF_MEDIA_AT_SLOT_ONE.current =
+          INDEX_OF_MEDIA_AT_SLOT_ONE.current + 1 < slot_one.length
+            ? INDEX_OF_MEDIA_AT_SLOT_ONE.current + 1
+            : 0;
+        ELAPSED_TIME_AT_SLOT_ONE.current = 0;
+      }
+    }
+
+    if (slot_two.length > 0) {
+      const currentMediaAtSlotTwo =
+        slot_two[INDEX_OF_MEDIA_AT_SLOT_TWO.current];
+
+      if (
+        ELAPSED_TIME_AT_SLOT_TWO.current + 1 <
+        currentMediaAtSlotTwo.duration
+      ) {
+        ELAPSED_TIME_AT_SLOT_TWO.current += 1;
+      } else {
+        INDEX_OF_MEDIA_AT_SLOT_TWO.current =
+          INDEX_OF_MEDIA_AT_SLOT_TWO.current + 1 < slot_two.length
+            ? INDEX_OF_MEDIA_AT_SLOT_TWO.current + 1
+            : 0;
+        ELAPSED_TIME_AT_SLOT_TWO.current = 0;
+      }
+    }
+
+    // if ((adsRef.current.slot_two || []).length > 0) {
+    //   const currentMediaAtSlotTwo =
+    //     adsRef.current.slot_one[INDEX_OF_MEDIA_AT_SLOT_TWO];
+
+    //   if (ELAPSED_TIME_AT_SLOT_TWO + 1 < currentMediaAtSlotTwo.duration) {
+    //     ELAPSED_TIME_AT_SLOT_TWO.current += 1;
+    //   } else {
+    //     INDEX_OF_MEDIA_AT_SLOT_TWO.current =
+    //       INDEX_OF_MEDIA_AT_SLOT_TWO.current + 1 <
+    //       adsRef.current.slot_two.length
+    //         ? INDEX_OF_MEDIA_AT_SLOT_TWO.current + 1
+    //         : 0;
+    //     ELAPSED_TIME_AT_SLOT_TWO.current = 0;
+    //   }
+    // }
+  };
+
   useEffect(() => {
     getAds();
     getSettings();
@@ -71,20 +161,25 @@ function MainView() {
       fetchQueues();
     }, 1000);
 
-    const interval2 = window.setInterval(() => {
-      getAds();
-    }, 1000);
+    // const interval2 = window.setInterval(() => {
+    //   getAds();
+    // }, 1000);
 
     const interval3 = window.setInterval(() => {
       getSettings();
+    }, 1000);
+
+    const adsInterval = window.setInterval(() => {
+      checkLoop();
     }, 1000);
 
     return () => {
       window.clearInterval(interval);
 
       window.clearInterval(interval1);
-      window.clearInterval(interval2);
+      // window.clearInterval(interval2);
       window.clearInterval(interval3);
+      window.clearInterval(adsInterval);
     };
   }, []);
 
@@ -134,7 +229,9 @@ function MainView() {
                     )
                     .map((queue) => (
                       <tr key={Math.random()}>
-                        <td className="w-50 fs-2 py-3 text-start ps-3">{queue.offices}</td>
+                        <td className="w-50 fs-2 py-3 text-start ps-3">
+                          {queue.offices}
+                        </td>
                         <td className="fs-2 py-3">{queue.window_name}</td>
                         <td className="fs-2 py-3">{queue.number}</td>
                       </tr>
@@ -162,8 +259,118 @@ function MainView() {
             }}
           >
             <div className="d-flex flex-column">
-              {(ads || []).map((x) => (
-                <div style={{ height: "43vh", position: "relative" }} className="border border-2 border-dark">
+              {(ads.slot_one || []).length > 0 ? (
+                <>
+                  {ads.slot_one[INDEX_OF_MEDIA_AT_SLOT_ONE.current] &&
+                    ads.slot_one[INDEX_OF_MEDIA_AT_SLOT_ONE.current].path &&
+                    ["png", "jpeg", "jpg"].includes(
+                      ads.slot_one[INDEX_OF_MEDIA_AT_SLOT_ONE.current].path
+                        .split(".")
+                        .pop()
+                        .toLowerCase()
+                    ) && (
+                      <img
+                        src={`${API_URL}/media/${
+                          ads.slot_one[INDEX_OF_MEDIA_AT_SLOT_ONE.current].uuid
+                        }/preview`}
+                        className="img-fluid w-100 h-100"
+                        alt={
+                          ads.slot_one[INDEX_OF_MEDIA_AT_SLOT_ONE.current].name
+                        }
+                      />
+                    )}
+
+                  {ads.slot_one[INDEX_OF_MEDIA_AT_SLOT_ONE.current] &&
+                    ads.slot_one[INDEX_OF_MEDIA_AT_SLOT_ONE.current].path &&
+                    ["mp4", "mpeg"].includes(
+                      ads.slot_one[INDEX_OF_MEDIA_AT_SLOT_ONE.current].path
+                        .split(".")
+                        .pop()
+                        .toLowerCase()
+                    ) && (
+                      <video
+                        width="100%"
+                        height="360"
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "fill",
+                        }}
+                        muted
+                        autoPlay
+                        loop
+                      >
+                        <source
+                          src={`${API_URL}/media/${
+                            ads.slot_one[INDEX_OF_MEDIA_AT_SLOT_ONE.current]
+                              .uuid
+                          }/preview`}
+                        ></source>
+                      </video>
+                    )}
+                </>
+              ) : (
+                <></>
+              )}
+
+              {(ads.slot_two || []).length > 0 ? (
+                <>
+                  {ads.slot_two[INDEX_OF_MEDIA_AT_SLOT_TWO.current] &&
+                    ads.slot_two[INDEX_OF_MEDIA_AT_SLOT_TWO.current].path &&
+                    ["png", "jpeg", "jpg"].includes(
+                      ads.slot_two[INDEX_OF_MEDIA_AT_SLOT_TWO.current].path
+                        .split(".")
+                        .pop()
+                        .toLowerCase()
+                    ) && (
+                      <img
+                        src={`${API_URL}/media/${
+                          ads.slot_two[INDEX_OF_MEDIA_AT_SLOT_TWO.current].uuid
+                        }/preview`}
+                        className="img-fluid w-100 h-100"
+                        alt={
+                          ads.slot_two[INDEX_OF_MEDIA_AT_SLOT_TWO.current].name
+                        }
+                      />
+                    )}
+
+                  {ads.slot_two[INDEX_OF_MEDIA_AT_SLOT_TWO.current] &&
+                    ads.slot_two[INDEX_OF_MEDIA_AT_SLOT_TWO.current].path &&
+                    ["mp4", "mpeg"].includes(
+                      ads.slot_two[INDEX_OF_MEDIA_AT_SLOT_TWO.current].path
+                        .split(".")
+                        .pop()
+                        .toLowerCase()
+                    ) && (
+                      <video
+                        width="100%"
+                        height="360"
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "fill",
+                        }}
+                        muted
+                        autoPlay
+                        loop
+                      >
+                        <source
+                          src={`${API_URL}/media/${
+                            ads.slot_two[INDEX_OF_MEDIA_AT_SLOT_TWO.current]
+                              .uuid
+                          }/preview`}
+                        ></source>
+                      </video>
+                    )}
+                </>
+              ) : (
+                <></>
+              )}
+              {/* {(ads.slot_one || []).map((x) => (
+                <div
+                  style={{ height: "43vh", position: "relative" }}
+                  className="border border-2 border-dark"
+                >
                   {["png", "jpeg", "jpg"].includes(
                     x.path.split(".").pop().toLowerCase()
                   ) && (
@@ -178,8 +385,8 @@ function MainView() {
                     x.path.split(".").pop().toLowerCase()
                   ) && (
                     <video
-                      width='100%'
-                      height='360'
+                      width="100%"
+                      height="360"
                       style={{
                         width: "100%",
                         height: "100%",
@@ -195,7 +402,7 @@ function MainView() {
                     </video>
                   )}
                 </div>
-              ))}
+              ))} */}
               {/* <div
                 style={{ height: "45vh" }}
                 className="d-flex justify-content-center align-items-center bg-secondary"
